1c61efbc8df8cf1866696a8d59f73e12542f539f
Roman Karpievich <karpievich@mail.scand>
2019-04-09 15:43:07 +0300

    Change project structure for iOS.

--- iOS/IAP/Example/Pods/Target Support Files/IAP/IAP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IAP : NSObject
-@end
-@implementation PodsDummy_IAP
-@end
---
+++ iOS/IAP/IAP.h
@@ -0,0 +1,17 @@
+//
+//  IAP.h
+//  IAP
+//
+//  Created by Roman Karpievich on 4/9/19.
+//  Copyright © 2019 Roman Karpievich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IAP.
+FOUNDATION_EXPORT double IAPVersionNumber;
+
+//! Project version string for IAP.
+FOUNDATION_EXPORT const unsigned char IAPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IAP/PublicHeader.h>
