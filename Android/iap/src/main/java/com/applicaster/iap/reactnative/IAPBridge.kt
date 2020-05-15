cbad11e07927bff589c49237dd473356dfbe98b7
kononenkoAnton <a.kononenko@applicaster.com>
2020-05-15 12:27:35 -0400

    Cjange grandle

--- Android/iap/src/main/java/com/applicaster/iap/reactnative/IAPBridge.kt
+++ Android/iap/src/main/java/com/applicaster/iap/reactnative/IAPBridge.kt
@@ -1,53 +1,12 @@
-package com.applicaster.quickbrickinplayer.reactnative
+package com.applicaster.iap.reactnative
 
-import com.applicaster.quickbrickinplayer.implementation.Accounts.InPlayerAuthorizationModelExt
-import com.applicaster.quickbrickinplayer.implementation.Assets.InPlayerItemAccessExt
-import com.applicaster.quickbrickinplayer.implementation.InPlayerPlugin.Companion.inPlayerSDKInitialization
-import com.applicaster.quickbrickinplayer.utils.noCredentialsError
-import com.applicaster.quickbrickinplayer.utils.noExpectedPayloadParams
-import com.applicaster.reactnative.utils.ConversionUtils
 import com.facebook.react.bridge.*
-import com.sdk.inplayer.callback.InPlayerCallback
-import com.sdk.inplayer.configuration.InPlayer
 
-enum class InPlayerAssetBridgeKeys(val shorthand: String) {
-    Id("id"),
-    EntryId("entryId");
-}
-
-class InPlayerAssetBridge(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
-    private val bridgeName = "InPlayerAssetBridge"
+class IAPBridge(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    private val bridgeName = "IAPBridge"
 
     override fun getName(): String {
         return bridgeName
     }
-
-    @ReactMethod
-    fun checkAccessForAsset(payload: ReadableMap?,
-                            promise: Promise?) {
-        try {
-            val jsonObject = ConversionUtils.readableMapToJson(payload)
-                    ?: throw IllegalArgumentException(noCredentialsError.message)
-            val id = jsonObject.getInt(InPlayerAssetBridgeKeys.Id.shorthand)
-
-            !inPlayerSDKInitialization(reactContext, jsonObject) && throw IllegalArgumentException(noCredentialsError.message)
-
-
-            val entryId = jsonObject.getString(InPlayerAssetBridgeKeys.EntryId.shorthand)
-
-            InPlayer.Assets.checkAccessForAsset(id, entryId,InPlayerCallback {itemAccess, error ->
-                if (error == null) {
-                    val inPlayerUserWrapper = InPlayerItemAccessExt.wrapToMap(itemAccess)
-                    promise?.resolve(inPlayerUserWrapper)
-                } else {
-                    promise?.reject(JSApplicationCausedNativeException(error.errorsList.joinToString(), error.e))
-                }
-            })
-
-        } catch (e: Exception) {
-            promise?.reject(JSApplicationCausedNativeException(e.message, e))
-        }
-    }
-
 
 }
--- Android/iap/src/main/java/com/applicaster/iap/reactnative/IAPPackage.kt
+++ Android/iap/src/main/java/com/applicaster/iap/reactnative/IAPPackage.kt
@@ -1,5 +1,4 @@
 package com.applicaster.iap.reactnative
-
 import android.view.View
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
@@ -7,15 +6,12 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
 
-class IAPPackage: ReactPackage {
+class IAPPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
-        return mutableListOf(
-                InPlayerAccountBridge(reactContext),
-                InPlayerAssetBridge(reactContext)
-        )
+        return mutableListOf(IAPBridge(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
-       return mutableListOf()
+        return mutableListOf()
     }
 }
