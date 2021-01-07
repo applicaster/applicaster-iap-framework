de1bf91a17bc85559a4e7d93d4be25c0a5f890b3
Alex Zchut <a.zchut@applicaster.com>
2021-01-07 14:07:33 +0200

    Updated to support purchase interruptions

---
+++ Specs/ApplicasterIAP/0.0.6/ApplicasterIAP.podspec
@@ -0,0 +1,13 @@
+Pod::Spec.new do |s|
+  s.name         = 'ApplicasterIAP'
+  s.version      = '0.0.6'
+  s.summary      = 'In App Purchases framework'
+  s.license      = 'MIT'
+  s.homepage     = 'https://github.com/applicaster/applicaster-iap-framework'
+  s.author       = { 'Roman Karpievich' => 'r.karpievich@applicaster.com' }
+  s.ios.deployment_target = '9.0'
+  s.swift_version = '5.1'
+  s.source       = { :git => "https://github.com/applicaster/applicaster-iap-framework", :tag => s.version }
+  s.source_files = 'iOS/ApplicasterIAP/*.{swift}'
+  s.requires_arc = true
+end
--- iOS/ApplicasterIAP/StoreObserver.swift
+++ iOS/ApplicasterIAP/StoreObserver.swift
@@ -10,85 +10,106 @@ import Foundation
 import StoreKit
 
 class StoreObserver: NSObject, SKPaymentTransactionObserver {
-    
     typealias PurchaseCompletion = (PurchaseResult) -> Void
     typealias RestoreCompletion = (RestoreResult) -> Void
     typealias DownloadsCompletion = ([SKDownload]) -> Void
-    
+
     private var activePurchases: [String: (Purchase, PurchaseCompletion)] = [:]
-    
+
     private var restoredPurchases: [SKPaymentTransaction] = []
     private var restoreCompletion: RestoreCompletion?
     private var finishing: Bool = true
-    
+
     public var downloadsCompletion: DownloadsCompletion?
     public var unfinishedTransactionCompletion: ((SKPaymentTransaction) -> Void)?
-    
+
     override init() {
         super.init()
         SKPaymentQueue.default().add(self)
     }
-    
+
     deinit {
         SKPaymentQueue.default().remove(self)
     }
-    
+
     // MARK: - Public methods
-    
+
     public func buy(_ purchase: Purchase, completion: @escaping (PurchaseResult) -> Void) {
         let payment = SKMutablePayment(product: purchase.item)
         payment.quantity = purchase.amount
-        
+
         SKPaymentQueue.default().add(payment)
-        
+
         activePurchases[purchase.item.productIdentifier] = (purchase, completion)
     }
-    
+
     public func restore(finishing: Bool = true, completion: @escaping RestoreCompletion) {
-        self.restoreCompletion = completion
+        restoreCompletion = completion
         self.finishing = finishing
         SKPaymentQueue.default().restoreCompletedTransactions()
     }
-    
+
     // MARK: - Private methods
-    
+
     private func purchased(transaction: SKPaymentTransaction) {
         let identifier = transaction.payment.productIdentifier
         guard let (purchase, completion) = activePurchases[identifier] else {
             return
         }
         activePurchases.removeValue(forKey: identifier)
-        
+
         purchase.transaction = transaction
-        
+
         let result = PurchaseResult.success(purchase)
         DispatchQueue.main.async {
             completion(result)
         }
-        
+
         if purchase.finishing == true {
             SKPaymentQueue.default().finishTransaction(transaction)
         }
     }
-    
+
     private func failed(transaction: SKPaymentTransaction) {
         let identifier = transaction.payment.productIdentifier
         guard let (_, completion) = activePurchases[identifier] else {
             unfinished(transaction: transaction)
             return
         }
-        activePurchases.removeValue(forKey: identifier)
-        
-        guard let error = transaction.error else {
+
+        guard let error = transaction.error,
+              shouldSkipFailedTransaction(forError: error) == false else {
             return
         }
-        
+
+        activePurchases.removeValue(forKey: identifier)
+
         let result = PurchaseResult.failure(error)
         DispatchQueue.main.async {
             completion(result)
         }
     }
-    
+
+    private func shouldSkipFailedTransaction(forError error: Error) -> Bool {
+        struct ErrorCodes {
+            static let code3038 = 3038 //Apple Media Services Terms and Conditions have changed. https://developer.apple.com/forums/thread/665933
+            static let code2024 = 2024 //Verification Required https://developer.apple.com/forums/thread/651407
+        }
+
+        let containsErrorCodes = [
+            ErrorCodes.code3038,
+            ErrorCodes.code2024
+        ]
+
+        let userInfo = (error as NSError).userInfo
+        guard let underlyingError = userInfo[NSUnderlyingErrorKey] as? NSError else {
+            return false
+        }
+
+        let foundErrorCodes = containsErrorCodes.map { underlyingError.code == $0 }
+        return foundErrorCodes.count > 0
+    }
+
     private func restored(transaction: SKPaymentTransaction) {
         if restoreCompletion == nil {
             unfinished(transaction: transaction)
@@ -96,14 +117,14 @@ class StoreObserver: NSObject, SKPaymentTransactionObserver {
             restoredPurchases.append(transaction)
         }
     }
-    
+
     private func unfinished(transaction: SKPaymentTransaction) {
         unfinishedTransactionCompletion?(transaction)
         SKPaymentQueue.default().finishTransaction(transaction)
     }
-    
+
     // MARK: - SKPaymentTransactionObserver methods
-    
+
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
         for transaction in transactions {
             switch transaction.transactionState {
@@ -120,12 +141,12 @@ class StoreObserver: NSObject, SKPaymentTransactionObserver {
             }
         }
     }
-    
+
     func paymentQueue(_ queue: SKPaymentQueue, restoreCompletedTransactionsFailedWithError error: Error) {
         restoreCompletion?(.failure(error))
         restoreCompletion = nil
     }
-    
+
     func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
         restoreCompletion?(.success(restoredPurchases))
 
@@ -134,11 +155,11 @@ class StoreObserver: NSObject, SKPaymentTransactionObserver {
                 SKPaymentQueue.default().finishTransaction(purchase)
             }
         }
-        
+
         restoredPurchases.removeAll()
         restoreCompletion = nil
     }
-    
+
     func paymentQueue(_ queue: SKPaymentQueue, updatedDownloads downloads: [SKDownload]) {
         downloadsCompletion?(downloads)
     }
